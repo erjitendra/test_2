3dacf3bac1beead8f79c2907fbb89c2241f59243
erjitendra <jitendrashahar@gmail.com>
2017-07-04 23:14:44 +0530

    comment added

--- app/src/main/java/com/example/jindal/dharam/MainActivity_Test.java
+++ app/src/main/java/com/example/jindal/dharam/MainActivity_Test.java
@@ -9,5 +9,6 @@ public class MainActivity_Test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Comment added
     }
 }
